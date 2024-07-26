00f5e85a5762fa94d9e40d5ed68036542b2b7d3e
DoubleNico <tomescuvlad123@gmail.com>
2024-07-26 18:56:45 +0300

    Fixed scoreboard lines packet with new wrapper

--- HypeGradients-Build/src/main/java/me/doublenico/hypegradients/packets/scoreboard/ScoreboardTeamPacket.java
+++ HypeGradients-Build/src/main/java/me/doublenico/hypegradients/packets/scoreboard/ScoreboardTeamPacket.java
@@ -5,6 +5,7 @@ import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import com.comphenix.protocol.wrappers.WrappedTeamParameters;
 import dev.perryplaysmc.dynamicconfigurations.utils.DynamicConfigurationDirectory;
 import me.doublenico.hypegradients.HypeGradients;
 import me.doublenico.hypegradients.api.MessageDetection;
@@ -18,8 +19,9 @@ import me.doublenico.hypegradients.api.event.MessageType;
 import me.doublenico.hypegradients.api.packet.MessagePacket;
 import me.doublenico.hypegradients.wrappers.scoreboard.WrapperScoreboardTeam;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Optional;
 
 public class ScoreboardTeamPacket extends MessagePacket {
     public ScoreboardTeamPacket(JavaPlugin plugin, ChatDetectionConfiguration chatDetectionConfiguration, ListenerPriority priority, PacketType type, MessageType messageType) {
@@ -33,52 +35,57 @@ public class ScoreboardTeamPacket extends MessagePacket {
     public void onPacketSending(PacketEvent event) {
         PacketContainer packet = event.getPacket();
         WrapperScoreboardTeam wrapper = new WrapperScoreboardTeam(packet);
-        if (wrapper.getPrefix().isEmpty())
-            return;
-        if (wrapper.getSuffix().isEmpty())
-            return;
-        WrappedChatComponent prefix = wrapper.getPrefix().get();
-        WrappedChatComponent suffix = wrapper.getSuffix().get();
+        Optional<WrappedTeamParameters> teamParameters = wrapper.getTeamParameters();
+        if(teamParameters.isEmpty()) return;
+        WrappedTeamParameters wrappedTeamParameters = teamParameters.get();
+        WrappedChatComponent displayName = wrapper.getDisplayName(wrappedTeamParameters);
+        WrappedChatComponent prefix = wrapper.getPrefix(wrappedTeamParameters);
+        WrappedChatComponent suffix = wrapper.getSuffix(wrappedTeamParameters);
+        if(displayName == null || prefix == null || suffix == null) return;
+        String displayNameText = new ChatJson(displayName.getJson()).convertToString();
         String prefixText = new ChatJson(prefix.getJson()).convertToString();
         String suffixText = new ChatJson(suffix.getJson()).convertToString();
-        if (prefixText == null || suffixText == null)
-            return;
-        Player player = event.getPlayer();
-        String text = prefixText + suffixText;
-        text = text.replace("§f", "");
-        String message = new ChatJson(text).convertToJson();
-        WrappedChatComponent component = WrappedChatComponent.fromText(text);
-        MessagePacketEvent messagePacketEvent = new MessagePacketEvent(player, getMessageType(), text, message, component);
+        WrapperScoreboardTeam newWrapperScoreboardTeam = new WrapperScoreboardTeam().builder(wrappedTeamParameters);
+        changeText(event, newWrapperScoreboardTeam, 0, displayName, displayNameText);
+        changeText(event, newWrapperScoreboardTeam, 1, prefix, prefixText);
+        changeText(event, newWrapperScoreboardTeam, 2, suffix, suffixText);
+        wrappedTeamParameters = newWrapperScoreboardTeam.build();
+        wrapper.setTeamParameters(wrappedTeamParameters);
+    }
+
+    // computer science breaks me ;(, priority 0, is for display name, 1 is for prefix, 2 is for suffix
+    public void changeText(PacketEvent event, WrapperScoreboardTeam scoreboardTeam, int priority, WrappedChatComponent initial, String text){
+        MessagePacketEvent messagePacketEvent = new MessagePacketEvent(event.getPlayer(), MessageType.SCOREBOARD_TEAM, text, initial.getJson(), initial);
         Bukkit.getPluginManager().callEvent(messagePacketEvent);
-        message = messagePacketEvent.getJsonMessage();
+        initial = messagePacketEvent.getChatComponent();
+        String json = messagePacketEvent.getJsonMessage();
         text = messagePacketEvent.getPlainMessage();
-        component = messagePacketEvent.getChatComponent();
         ChatGradient gradient = new ChatGradient(text);
-        if (gradient.isGradientTeam() && getChatDetectionConfiguration().getChatDetectionValues().scoreboardTitle()) {
-            GradientModifyEvent gradientModifyEvent = new GradientModifyEvent(player, text, message, gradient.getMessage(), getMessageType());
+        if(gradient.isGradient() && getChatDetectionConfiguration().getChatDetectionValues().scoreboardLines()){
+            GradientModifyEvent gradientModifyEvent = new GradientModifyEvent(event.getPlayer(), text, json, gradient.getMessage(), getMessageType());
             Bukkit.getPluginManager().callEvent(gradientModifyEvent);
             gradient = new ChatGradient(gradientModifyEvent.getGradientMessage());
-            prefix.setJson((new ChatJson(gradient.translateGradient()).convertToJson()));
-            wrapper.setPrefix(prefix);
-            wrapper.setSuffix(WrappedChatComponent.fromText(""));
+            initial.setJson(new ChatJson(gradient.translateGradient()).convertToJson());
+            if(priority == 0) scoreboardTeam.setDisplayName(initial);
+            if(priority == 1) scoreboardTeam.setPrefix(initial);
+            if(priority == 2) scoreboardTeam.setSuffix(initial);
             if (((HypeGradients) getPlugin()).getMetricsWrapper() == null) return;
             ((HypeGradients) getPlugin()).getMetricsWrapper().gradientChart();
             ((HypeGradients) getPlugin()).getMetricsWrapper().gradientDetectionChart("Scoreboard", "Team");
         }
         if (((HypeGradients) getPlugin()).getMessageDetectionConfig().getChatDetectionValues().scoreboardTitle()) {
             for (MessageDetection messageDetection : MessageDetectionManager.getInstance().getMessageDetectionList()) {
-                if (!messageDetection.isEnabled(event.getPlayer(), text, message, component)) continue;
+                if (!messageDetection.isEnabled(event.getPlayer(), text, json, initial)) continue;
                 HypeGradients plugin = JavaPlugin.getPlugin(HypeGradients.class);
                 ChatDetectionConfiguration chatDetectionConfiguration = messageDetection.chatDetectionConfiguration(event.getPlayer(), new DynamicConfigurationDirectory(plugin, plugin.getDataFolder()));
                 if (!chatDetectionConfiguration.getChatDetectionValues().scoreboardLines()) continue;
                 text = messageDetection.getPlainMessage(event.getPlayer(), text);
-                prefix.setJson((new ChatJson(text).convertToJson()));
-                wrapper.setPrefix(prefix);
-                wrapper.setSuffix(WrappedChatComponent.fromText(""));
+                initial.setJson(new ChatJson(text).convertToJson());
+                if (priority == 0) scoreboardTeam.setDisplayName(initial);
+                if (priority == 1) scoreboardTeam.setPrefix(initial);
+                if (priority == 2) scoreboardTeam.setSuffix(initial);
             }
-            prefix.setJson((new ChatJson(text).convertToJson()));
-            wrapper.setPrefix(prefix);
-            wrapper.setSuffix(WrappedChatComponent.fromText(""));
         }
+
     }
 }
--- HypeGradients-Build/src/main/java/me/doublenico/hypegradients/wrappers/scoreboard/WrapperScoreboardTeam.java
+++ HypeGradients-Build/src/main/java/me/doublenico/hypegradients/wrappers/scoreboard/WrapperScoreboardTeam.java
@@ -2,14 +2,24 @@ package me.doublenico.hypegradients.wrappers.scoreboard;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.EnumWrappers;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import com.comphenix.protocol.wrappers.WrappedTeamParameters;
 import me.doublenico.hypegradients.api.packet.AbstractPacket;
 
 import java.util.Optional;
 
 public class WrapperScoreboardTeam extends AbstractPacket {
     public static final PacketType TYPE =
-            PacketType.Play.Server.SCOREBOARD_TEAM;
+        PacketType.Play.Server.SCOREBOARD_TEAM;
+    public WrappedChatComponent displayName;
+    public WrappedChatComponent prefix;
+    public WrappedChatComponent suffix;
+    public String nametagVisibility;
+    public String collisionRule;
+    public EnumWrappers.ChatFormatting color;
+    public int options;
+    public WrappedTeamParameters.Builder teamParameters;
 
     public WrapperScoreboardTeam() {
         super(new PacketContainer(TYPE), TYPE);
@@ -20,42 +30,97 @@ public class WrapperScoreboardTeam extends AbstractPacket {
         super(packet, TYPE);
     }
 
-
-    /**
-     * Retrieve Team Display Name.
-     * <p>
-     * Notes: only if Mode = 0 or 2.
-     *
-     * @return The current Team Display Name
-     */
-    public Optional<WrappedChatComponent> getDisplayName() {
-        if (handle.getOptionalStructures().read(0) == null) return Optional.empty();
-        return handle.getOptionalStructures().read(0).map((structure) -> structure.getChatComponents().read(0));
+    public Optional<WrappedTeamParameters> getTeamParameters() {
+        if (!WrappedTeamParameters.isSupported()) return Optional.empty();
+        if  (handle.getOptionalTeamParameters().readSafely(0).isEmpty()) return Optional.empty();
+        return handle.getOptionalTeamParameters().read(0);
     }
 
-    /**
-     * Set Team Display Name.
-     *
-     * @param value - new value.
-     */
-    public void setDisplayName(WrappedChatComponent value) {
-        handle.getOptionalStructures().read(0).map((structure) -> structure.getChatComponents().write(0, value));
+    public void setTeamParameters(WrappedTeamParameters wrappedTeamParameters) {
+        if (!WrappedTeamParameters.isSupported()) return;
+        handle.getOptionalTeamParameters().write(0, Optional.of(wrappedTeamParameters));
     }
 
-    public Optional<WrappedChatComponent> getPrefix() {
-        return handle.getOptionalStructures().read(0).map((structure) -> structure.getChatComponents().read(1));
+    public WrappedChatComponent getDisplayName(WrappedTeamParameters wrappedTeamParameters) {
+        if (!WrappedTeamParameters.isSupported()) return null;
+        return wrappedTeamParameters.getDisplayName();
     }
 
-    public void setPrefix(WrappedChatComponent value) {
-        handle.getOptionalStructures().read(0).map((structure) -> structure.getChatComponents().write(1, value));
+
+    public WrappedChatComponent getPrefix(WrappedTeamParameters wrappedTeamParameters) {
+        if (!WrappedTeamParameters.isSupported()) return null;
+        return wrappedTeamParameters.getPrefix();
     }
 
-    public Optional<WrappedChatComponent> getSuffix() {
-        return handle.getOptionalStructures().read(0).map((structure) -> structure.getChatComponents().read(2));
+
+    public WrappedChatComponent getSuffix(WrappedTeamParameters wrappedTeamParameters) {
+        if (!WrappedTeamParameters.isSupported()) return null;
+        return wrappedTeamParameters.getSuffix();
     }
 
-    public void setSuffix(WrappedChatComponent value) {
-        handle.getOptionalStructures().read(0).map((structure) -> structure.getChatComponents().write(2, value));
+    public String getNametagVisibility(WrappedTeamParameters wrappedTeamParameters){
+        if (!WrappedTeamParameters.isSupported()) return null;
+        return wrappedTeamParameters.getNametagVisibility();
+    }
+
+    public String getCollisionRule(WrappedTeamParameters wrappedTeamParameters){
+        if (!WrappedTeamParameters.isSupported()) return null;
+        return wrappedTeamParameters.getCollisionRule();
+    }
+
+    public EnumWrappers.ChatFormatting getColor(WrappedTeamParameters wrappedTeamParameters){
+        if (!WrappedTeamParameters.isSupported()) return null;
+        return wrappedTeamParameters.getColor();
+    }
+
+    public int getOptions(WrappedTeamParameters wrappedTeamParameters, int options){
+        if (!WrappedTeamParameters.isSupported()) return 0;
+        return wrappedTeamParameters.getOptions();
+    }
+
+    public WrapperScoreboardTeam builder(WrappedTeamParameters wrappedTeamParameters){
+        teamParameters = WrappedTeamParameters.newBuilder(wrappedTeamParameters);
+        return this;
+    }
+
+    public WrapperScoreboardTeam setDisplayName(WrappedChatComponent displayName){
+        teamParameters.displayName(displayName);
+        return this;
+    }
+
+    public WrapperScoreboardTeam setPrefix(WrappedChatComponent prefix){
+        teamParameters.prefix(prefix);
+        return this;
+    }
+
+    public WrapperScoreboardTeam setSuffix(WrappedChatComponent suffix){
+        teamParameters.suffix(suffix);
+        return this;
+    }
+
+    public WrapperScoreboardTeam setNametagVisibility(String nametagVisibility){
+        teamParameters.nametagVisibility(nametagVisibility);
+        return this;
+    }
+
+    public WrapperScoreboardTeam setCollisionRule(String collisionRule){
+        teamParameters.collisionRule(collisionRule);
+        return this;
+    }
+
+    public WrapperScoreboardTeam setColor(EnumWrappers.ChatFormatting color){
+        teamParameters.color(color);
+        return this;
+    }
+
+    public WrapperScoreboardTeam setOptions(int options){
+        teamParameters.options(options);
+        return this;
+    }
+
+
+    public WrappedTeamParameters build(){
+        return teamParameters.build();
     }
 
 }
