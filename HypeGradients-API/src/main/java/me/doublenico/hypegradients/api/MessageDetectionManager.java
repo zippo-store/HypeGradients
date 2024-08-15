699b9f1410974c72dbd8aef544b0c8c0aa266d2b
DoubleNico <tomescuvlad123@gmail.com>
2024-08-16 00:56:26 +0300

    Added docs to the custom message detection api

--- HypeGradients-API/src/main/java/me/doublenico/hypegradients/api/MessageDetectionManager.java
+++ HypeGradients-API/src/main/java/me/doublenico/hypegradients/api/MessageDetectionManager.java
@@ -13,18 +13,35 @@ public class MessageDetectionManager {
         return MessageDetectionManager.InstanceHolder.instance;
     }
 
+    /**
+     * Get the list of {@link MessageDetection}
+     * @return the list of {@link MessageDetection}
+     */
     public List<MessageDetection> getMessageDetectionList() {
         return messageDetectionList;
     }
 
+    /**
+     * Add a message detection to the list of message detections
+     * @param messageDetection the custom message detection
+     */
     public void addMessageDetection(MessageDetection messageDetection) {
         messageDetectionList.add(messageDetection);
     }
 
+    /**
+     * Remove a message detection from the list of message detections
+     * @param messageDetection the custom message detection
+     */
     public void removeMessageDetection(MessageDetection messageDetection) {
         messageDetectionList.remove(messageDetection);
     }
 
+    /**
+     * Get a message detection from the list of message detections
+     * @param name the name of the message detection
+     * @return the message detection
+     */
     public MessageDetection getMessageDetection(String name) {
         for (MessageDetection messageDetection : messageDetectionList) {
             if (messageDetection.getName().equals(name))
