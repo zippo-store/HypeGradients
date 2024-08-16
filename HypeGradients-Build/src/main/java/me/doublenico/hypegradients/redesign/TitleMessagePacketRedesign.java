2af6c75ad0f6dd23c18940a853f7e6411d49dc20
DoubleNico <tomescuvlad123@gmail.com>
2024-08-16 17:39:07 +0300

    Added the configurations to the redesign

--- HypeGradients-Build/src/main/java/me/doublenico/hypegradients/redesign/TitleMessagePacketRedesign.java
+++ HypeGradients-Build/src/main/java/me/doublenico/hypegradients/redesign/TitleMessagePacketRedesign.java
@@ -4,53 +4,46 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import me.doublenico.hypegradients.HypeGradients;
 import me.doublenico.hypegradients.api.chat.ChatGradient;
 import me.doublenico.hypegradients.api.chat.ChatJson;
-import me.doublenico.hypegradients.api.detection.ChatDetectionConfiguration;
-import me.doublenico.hypegradients.api.event.MessagePacketEvent;
 import me.doublenico.hypegradients.api.event.MessageType;
 import me.doublenico.hypegradients.api.packet.MessagePacket;
+import me.doublenico.hypegradients.api.packet.components.MessagePacketComponents;
+import me.doublenico.hypegradients.api.packet.components.MessagePacketConfigurations;
 import me.doublenico.hypegradients.bstats.Metrics;
 import me.doublenico.hypegradients.wrappers.title.WrapperPlayServerTitle;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TitleMessagePacketRedesign extends MessagePacket {
 
-    public TitleMessagePacketRedesign(JavaPlugin plugin, ChatDetectionConfiguration chatDetectionConfiguration, ListenerPriority priority, PacketType type, MessageType messageType) {
-        super(plugin, chatDetectionConfiguration, priority, type, messageType);
+    public TitleMessagePacketRedesign(JavaPlugin plugin, MessagePacketConfigurations configurations, ListenerPriority priority, PacketType type, MessageType messageType) {
+        super(plugin, configurations, priority, type, messageType);
     }
 
     @Override
     public boolean register() {
-        return ((HypeGradients) getPlugin()).getSettingsConfig().getChatDetectionValues().title();
+        return getMessagePacketConfigurations().settings().title();
     }
 
     @Override
     public void onPacketSending(PacketEvent event) {
         PacketContainer packet = event.getPacket();
         WrapperPlayServerTitle wrapper = new WrapperPlayServerTitle(packet);
-        WrappedChatComponent component = wrapper.getTitle();
-        if (component == null)
+        if (wrapper.getTitle() == null)
             return;
+        MessagePacketComponents components = new MessagePacketComponents(wrapper.getTitle(), wrapper.getTitle().getJson(), (new ChatJson(wrapper.getTitle().getJson())).convertToString());
         Player player = event.getPlayer();
-        String message = component.getJson();
-        String string = (new ChatJson(message)).convertToString();
-        MessagePacketEvent messagePacketEvent = new MessagePacketEvent(player, getMessageType(), string, message, component);
-        Bukkit.getPluginManager().callEvent(messagePacketEvent);
-        if (messagePacketEvent.isCancelled()) return;
-        message = messagePacketEvent.getJsonMessage();
-        string = messagePacketEvent.getPlainMessage();
-        component = messagePacketEvent.getChatComponent();
-        ChatGradient gradient = new ChatGradient(string);
-        if (isGradient(gradient, getChatDetectionConfiguration().getChatDetectionValues().title())) {
-            component = setGradient(player, gradient, component, message, string);
+        components = getMessageEvent(player, components);
+        if (components == null) return;
+        ChatGradient gradient = new ChatGradient(components.getPlainMessage());
+        if (isGradient(gradient, getMessagePacketConfigurations().gradient().getChatDetectionValues().title())) {
+            components.setWrappedChatComponent(setGradient(player, gradient, components.getWrappedChatComponent(), components.getJsonMessage(), components.getPlainMessage()));
             new Metrics().setMetrics((HypeGradients) getPlugin(), "Title", "Title");
         }
-        wrapper.setTitle(component);
+
+        wrapper.setTitle(components.getWrappedChatComponent());
     }
 
 }
