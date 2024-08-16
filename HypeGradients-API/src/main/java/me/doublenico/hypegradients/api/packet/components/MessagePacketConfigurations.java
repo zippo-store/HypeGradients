4505207fc7321adb040ef719c8785e7fae77b034
DoubleNico <tomescuvlad123@gmail.com>
2024-08-16 17:38:33 +0300

    Remade MessagePacketConfigurations to hold detection values

--- HypeGradients-API/src/main/java/me/doublenico/hypegradients/api/packet/components/MessagePacketConfigurations.java
+++ HypeGradients-API/src/main/java/me/doublenico/hypegradients/api/packet/components/MessagePacketConfigurations.java
@@ -1,11 +1,12 @@
 package me.doublenico.hypegradients.api.packet.components;
 
 import me.doublenico.hypegradients.api.detection.ChatDetectionConfiguration;
+import me.doublenico.hypegradients.api.detection.ChatDetectionValues;
 
 /**
- * Contains all the configurations for the message packets, the settings.yml, gradient.yml and messageDetection.yml, see {@link ChatDetectionConfiguration}.
- * @param settings The settings.yml configuration
- * @param gradient The gradient.yml configuration
- * @param messageDetection The messageDetection.yml configuration
+ * Contains the settings and messageDetection {@link ChatDetectionValues} and the gradient config {@link ChatDetectionConfiguration}.
+ * @param settings The settings values
+ * @param messageDetection The message detection values
+ * @param gradient The gradient configuration
  */
-public record MessagePacketConfigurations(ChatDetectionConfiguration settings, ChatDetectionConfiguration gradient, ChatDetectionConfiguration messageDetection) {}
+public record MessagePacketConfigurations(ChatDetectionValues settings, ChatDetectionValues messageDetection, ChatDetectionConfiguration gradient) {}
