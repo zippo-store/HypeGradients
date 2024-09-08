613cfea32ff4cb5d309bedc4eb58b663bf53a051
DoubleNico <tomescuvlad123@gmail.com>
2024-09-08 22:56:47 +0300

    New methods to make it easy to work with the API

--- HypeGradients-API/src/main/java/me/doublenico/hypegradients/api/packet/components/MessagePacketComponents.java
+++ HypeGradients-API/src/main/java/me/doublenico/hypegradients/api/packet/components/MessagePacketComponents.java
@@ -1,10 +1,13 @@
 package me.doublenico.hypegradients.api.packet.components;
 
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import me.doublenico.hypegradients.api.chat.ChatJson;
 
 /**
  * Represents the components of a message packet that can be modified
  * Contains the {@link WrappedChatComponent}, the json message, and the plain message of the packet
+ * <p>
+ * The {@link WrappedChatComponent} should be modified to change the message
  */
 public class MessagePacketComponents {
 
@@ -22,23 +25,43 @@ public class MessagePacketComponents {
         return wrappedChatComponent;
     }
 
-    public void setWrappedChatComponent(WrappedChatComponent wrappedChatComponent) {
-        this.wrappedChatComponent = wrappedChatComponent;
+    public String getPlainMessage() {
+        return plainMessage;
     }
 
     public String getJsonMessage() {
         return jsonMessage;
     }
 
+    public void setWrappedChatComponent(WrappedChatComponent wrappedChatComponent) {
+        this.wrappedChatComponent = wrappedChatComponent;
+    }
+
+    public void setStringWrappedChatComponent(String wrappedChatComponent) {
+        this.wrappedChatComponent = WrappedChatComponent.fromText(wrappedChatComponent);
+    }
+
+    public void setJsonWrappedChatComponent(String jsonMessage) {
+        this.wrappedChatComponent = WrappedChatComponent.fromJson(jsonMessage);
+    }
+
+    public void setLegacyWrappedChatComponent(String legacyMessage) {
+        this.wrappedChatComponent = WrappedChatComponent.fromLegacyText(legacyMessage);
+    }
+
     public void setJsonMessage(String jsonMessage) {
         this.jsonMessage = jsonMessage;
     }
 
-    public String getPlainMessage() {
-        return plainMessage;
+    public void setJsonMessage(WrappedChatComponent wrappedChatComponent) {
+        this.jsonMessage = wrappedChatComponent.getJson();
     }
 
     public void setPlainMessage(String plainMessage) {
         this.plainMessage = plainMessage;
+    }
+
+    public void setPlainMessage(WrappedChatComponent wrappedChatComponent) {
+        this.plainMessage = new ChatJson(wrappedChatComponent.getJson()).convertToString();
     }
 }
